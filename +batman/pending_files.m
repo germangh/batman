2601543080fe62b7447e996fbd881c506541912d
German Gomez-Herrero <german.gomezherrero@kasku.org>
2013-08-31 09:52:50 +0200

    pending_jobs realizes if a job has been already queued

--- +batman/pending_files.m
+++ +batman/pending_files.m
@@ -7,6 +7,14 @@ alreadyDone = cellfun(@(x) exist(x, 'dir')>0, meegpipeDirs);
 
 fileExists = cellfun(@(x) exist(x, 'file')>0, files);
 
-files = files(fileExists & ~alreadyDone);
+inQueue = false(size(files));
+for i = 1:numel(files)
+   jobName = fileparts(files{i});
+   [~, resp] = system(['qstat -j ' jobName]);
+   inQueue = ~isempty(strfind(resp, 'jobs do not exist'));
+end
+
+
+files = files(fileExists & ~alreadyDone & ~inQueue);
 
 end
