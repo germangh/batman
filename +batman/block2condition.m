378a29a0bff06c1b5405831b335028f7fe65c3ef
German Gomez-Herrero <german.gomezherrero@kasku.org>
2013-09-04 14:14:19 +0200

    block to condition mapping

---
+++ +batman/block2condition.m
@@ -0,0 +1,68 @@
+function [cond, condIDout] = block2condition(subj, blockID)
+% block2condition
+% ===
+%
+% Map block numbers to conditions for all experimental subjects
+%
+% ## Usage synopsis
+%
+% ````matlab
+% cond = block2condition(5, 10)
+% assert(strcmp(cond, 'dark-supine-warm'));
+% ````
+% 
+% See also: batman.ft_freqanalysis_aggregate
+
+import misc.dlmread;
+import mperl.file.spec.catfile;
+import batman.*;
+import mperl.join;
+
+if nargin < 2 || numel(blockID) ~= 1,
+    error('blockID must be a single block index');
+end
+
+fName = catfile(root_path, 'data', 'conditions.csv');
+[condSpecs, varNames, condID] = dlmread(fName, ',', 0, 1);
+varNames = varNames(2:end);
+
+% Create some nice condition names
+condNames = cell(size(condID));
+for i = 1:numel(condNames)
+    condNames{i} = '';
+    for j = 1:numel(varNames)
+        condNames{i} = [condNames{i} ...
+            varNames{j} num2str(condSpecs(i,j)) '_'];
+    end
+    condNames{i}(end) = [];
+end
+
+fName = catfile(root_path, 'data', 'protocol.csv');
+[prot, condID2] = dlmread(fName, ',');
+
+condID2 = condID2(3:end);
+subjID = prot(:,1);
+prot = prot(:, 3:end);
+
+[~, rowIdx] = ismember(subj, subjID); 
+
+if any(rowIdx < 1),
+    warning('The following are not valid subject IDs: %s', ...
+        join(', ', subj(rowIdx < 1)));
+end
+
+subj(rowIdx < 1)   = [];
+rowIdx(rowIdx < 1) = [];
+
+cond = cell(size(subj));
+condIDout = cell(size(subj));
+
+for i = 1:numel(subj)
+   condIDout{i} = condID2{prot(rowIdx(1), :) == blockID};
+   cond{i} = condNames{ismember(condID, condIDout{i})};
+end
+
+
+
+
+end
---
+++ +batman/ft_freqanalysis_aggregate.m
@@ -0,0 +1,32 @@
+function ftripStruct = ft_freqanalysis_aggregate(DIR)
+% ft_freqanalysis_aggregate
+% ====
+%
+% Aggregate .meegpipe results into a structure suitable for analysis with
+% Fieldtrip's ft_freqanalysis
+%
+% ## Usage synopsis
+%
+% ````matlab
+% rDir = '/data1/projects/batman/analysis/cleaning';
+% subj = [1, 3:5, 10];
+% [ftripStruct, cond] = ft_freqanalysis_aggregate(rDir, subj)
+% ````
+%
+% Where
+%
+% `rDir` is the root directory where the .meegpipe results are stored.
+%
+% `subj` is a numeric array of subject IDs. Only these subjects will be
+% considered in the aggregation.
+%
+% `ftripStruct` is a 2x2x2 cell array with Fieltrip structures that may be
+% introduced into `ft_freqanalysis`.
+%
+% `cond` is a 2x2x2 cell array with condition IDs as described by 
+% `batman.block2condition`.
+%
+% 
+% See also: batman.block2condition
+
+end
