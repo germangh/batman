c820c934d27ad1d8e79fb8b64177668d6f94bd46
German Gomez-Herrero <g@germangh.com>
2014-05-22 10:45:03 +0200

    Reorganize pupillator scripts

--- +pupillator/+pvt/aggregate.m
+++ +pupillator/+pvt/aggregate.m
@@ -9,7 +9,6 @@ function aggregate(inputDir, outputFile)
 import meegpipe.aggregate2;
 import mperl.file.find.finddepth_regex_match;
 import pupillator.*;
-import misc.get_hostname;
 import mperl.join;
 import mperl.file.spec.catfile;
 import mperl.file.spec.catdir;
@@ -19,21 +18,14 @@ if nargin < 1, inputDir = []; end
 if nargin < 2, outputFile = []; end
 
 %% Aggregation parameters
-switch lower(get_hostname),
-    case 'nin271'
-        BASE_PATH = 'D:\data\pupw';
-    otherwise
-        BASE_PATH = '/data1/projects/batman/analysis/pupillator';
-end
+BASE_PATH = '/data1/projects/batman/analysis/pupillator/pvt';
 
 if isempty(outputFile),
     outputFile = catfile(BASE_PATH, 'pvt_features');
 end
 
 if isempty(inputDir),
-    inputDir = dir(BASE_PATH, 'pvt_\d\d\d\d\d\d-\d\d\d\d\d\d$');
-    inputDir = sort(inputDir);
-    inputDir = catdir(BASE_PATH, inputDir{end});
+    inputDir = misc.find_latest_dir(BASE_PATH);
 end
 
 % How to translate the file names into info tags
@@ -46,9 +38,7 @@ SUBJECTS = 1:12;
 % List of blocks to be aggregated
 BLOCKS = 1:2;
 
-
 %% Do the aggregation
-
 regex = ['0+(' join('|', SUBJECTS) ')_pupillometry.+_(' join('|', BLOCKS) ...
     ').csv$'];
 files = finddepth_regex_match(inputDir, regex, true);
--- +pupillator/+pvt/main.m
+++ +pupillator/+pvt/main.m
@@ -2,7 +2,6 @@
 
 import physioset.event.class_selector;
 import somsds.link2rec;
-import misc.get_hostname;
 import misc.regexpi_dir;
 import mperl.join;
 import pupillator.*;
@@ -10,25 +9,20 @@ import pupillator.*;
 %% User parameters
 USE_OGE   = true;
 DO_REPORT = true;
-QUEUE     = 'short.q';
+QUEUE     = 'short.q@somerenserver.herseninstituut.knaw.nl';
 subjects  = 1:12;
 
 %% Link (or find the location of) the relevant .csv files
 regex = ['(' join('|', subjects) ')'];
 regex = [regex '.+(supine|sitting)_\d.csv$'];
 
-switch lower(get_hostname),
-    case 'nin271',
-        files = regexpi_dir('D:/data/pupw', regex);
-    otherwise
-          folder = ['/data1/projects/batman/analysis/pupillator/pvt_' ...
-            datestr(now, 'yymmdd-HHMMSS')];
-        files = link2rec('pupw', 'modality', 'pupillometry', ...
-            'file_ext', '.csv', ...
-            'cond_regex', '(morning|afternoon)', ...
-            'folder', folder, ...
-            'subject', subjects);
-end
+folder = ['/data1/projects/batman/analysis/pupillator/pvt/' ...
+    datestr(now, 'yymmdd-HHMMSS')];
+files = link2rec('pupw', 'modality', 'pupillometry', ...
+    'file_ext', '.csv', ...
+    'cond_regex', '(morning|afternoon)', ...
+    'folder', folder, ...
+    'subject', subjects);
 
 %% Process all files with the pvt_analysis pipeline
 myPipe = pipes.pvt_analysis(...
